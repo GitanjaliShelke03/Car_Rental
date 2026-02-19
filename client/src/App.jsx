28d592f881c4017d52d0f65454b92410fe3d2f56
GitanjaliShelke03 <gitshelke@gmail.com>
2026-02-19 18:37:35 +0530

    Navbar added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
+
+  const[setLogin,setShowLogin] = useState(false)
+const isOwnerPath = useLocation().pathname.startWith('/') 
+
   return (
-    <div></div>
+    
+    <>
+    {isOwnerPath && <Navbar setShowLogin={setShowLogin} /> }   {/*shows navbar on every page */}
+    </>
   )
 }
 
